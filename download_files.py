2ab08aeed97f1f81612777ad6eb51fba1b353e54
Cameron Falls <camfalls0201@gmail.com>
2022-02-15 12:56:17 -0600

    Initial download script

---
+++ download_files.py
@@ -0,0 +1,7 @@
+import internetarchive
+
+identifier = "" # identifier of item to download from
+output_loc = "C:\\" # output directory on local disk
+glob_str = r"" # glob pattern to filter items to download
+
+internetarchive.download(identifier, destdir=output_loc, verbose=True, ignore_existing=True, glob_pattern=glob_str)
